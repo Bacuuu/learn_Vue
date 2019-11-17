ca22f558094f6735ee759fad176f67dd52b46bdc
Attention355 <823727097@qq.com>
2019-11-17 22:32:04 +0800

    修改为新的路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import GoodsList from '@/views/GoodsList'
-import Title from '@/views/Title'
-import Image from '@/views/Image'
-import Cart from '@/views/Cart'
-import Axios from '@/components/axios'
+// import HelloWorld from '@/components/HelloWorld'
+// import GoodsList from '@/views/GoodsList'
+// import Title from '@/views/Title'
+// import Image from '@/views/Image'
+// import Cart from '@/views/Cart'
+// import Axios from '@/components/axios'
+import PGoodsList from '../views/PGoodsList'
 
 Vue.use(Router)
 
@@ -13,54 +14,57 @@ export default new Router({
   // 指定为history模式
   // hash模式 /#/
   mode: 'history',
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      components: {
-        default: HelloWorld,
-        title: Title,
-        image: Image
-      }
-    },
-    {
-      path: '/axios',
-      name: 'Axios',
-      component: Axios
-    },
-    {
-      path: '/goods',
-      name: 'GoodsList',
-      components: {
-        default: GoodsList,
-        img: Image
-      }
-      // children: [
-      //   {
-      //     path: 'title',
-      //     name: 'title',
-      //     component: Title
-      //   },
-      //   {
-      //     path: 'image',
-      //     name: 'image',
-      //     component: Image
-      //   }
-      // ]
-    },
-    {
-      path: '/goods/apple',
-      name: 'goods_apple',
-      components: {
-        default: Image,
-        title: Title
-      }
-    },
-    {
-      path: '/cart/:cartID',
-      name: 'cart',
-      component: Cart
-    }
+  routes: [{
+    path: '/',
+    component: PGoodsList
+  }
+    // {
+    //   path: '/',
+    //   name: 'HelloWorld',
+    //   components: {
+    //     default: HelloWorld,
+    //     title: Title,
+    //     image: Image
+    //   }
+    // },
+    // {
+    //   path: '/axios',
+    //   name: 'Axios',
+    //   component: Axios
+    // },
+    // {
+    //   path: '/goods',
+    //   name: 'GoodsList',
+    //   components: {
+    //     default: GoodsList,
+    //     img: Image
+    //   }
+    //   // children: [
+    //   //   {
+    //   //     path: 'title',
+    //   //     name: 'title',
+    //   //     component: Title
+    //   //   },
+    //   //   {
+    //   //     path: 'image',
+    //   //     name: 'image',
+    //   //     component: Image
+    //   //   }
+    //   // ]
+    // },
+    // {
+    //   path: '/goods/apple',
+    //   name: 'goods_apple',
+    //   components: {
+    //     default: Image,
+    //     title: Title
+    //   }
+    // },
+    // {
+    //   path: '/cart/:cartID',
+    //   name: 'cart',
+    //   component: Cart
+    // }
     // {
     //   path: '/cart',
     //   name: 'cart',
