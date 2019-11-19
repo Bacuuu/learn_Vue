5cac8982f435bba448fbf235b1df7a1156bf9977
Attention355 <823727097@qq.com>
2019-11-19 23:06:05 +0800

    首页修改，提取样式等

--- src/views/PGoodsList.vue
+++ src/views/PGoodsList.vue
@@ -5,14 +5,18 @@
       <span slot="first">/Goods</span>
       <span slot="second">/Apple</span>
     </nav-bread>
-    <div style='display:flex;flex-direction:row;flex-wrap:wrap;'>
-    <nav-goods-card v-for='card in data' :data='card' :key='card.name'></nav-goods-card>
+    <div class="cardList">
+      <div class='priceFilter'>
+        <a href="javascript:;" v-for='price in priceFilter' :key=price>{{price.startPrice}}--{{price.endPrice}}</a>
+      </div>
+      <nav-goods-card v-for='card in data' :data='card' :key='card.name'></nav-goods-card>
     </div>
     <nav-footer></nav-footer>
   </div>
 </template>
 
 <script>
+import '@/assets/css/index.css'
 import NavHeader from '@/components/Header'
 import NavFooter from '@/components/Footer'
 import NavBread from '@/components/bread'
@@ -20,7 +24,25 @@ import NavGoodsCard from '@/components/goodscard'
 export default {
   data () {
     return {
-      data: {}
+      data: {},
+      priceFilter: [
+        {
+          startPrice: 1499,
+          endPrice: 2499
+        },
+        {
+          startPrice: 2499,
+          endPrice: 3499
+        },
+        {
+          startPrice: 3499,
+          endPrice: 4499
+        },
+        {
+          startPrice: 4499,
+          endPrice: 4999
+        }
+      ]
     }
   },
   mounted: function () {
