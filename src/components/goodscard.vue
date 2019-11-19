ce546d41975636f3f9d4f9a90582ab6d991fa6e9
Attention355 <823727097@qq.com>
2019-11-19 21:16:46 +0800

    背景颜色方式修改为图片显示

--- src/components/goodscard.vue
+++ src/components/goodscard.vue
@@ -1,20 +1,25 @@
 <template>
   <div class="goodsCard">
-    <div class='goodsImg' :style='"background:"+data.color'>
-    </div>
-    <span>{{data.name}}</span>
-    <span>{{data.price}}</span>
+    <!-- <div class='goodsImg' :style='"background:"+data.color'> -->
+      <img :src='imgSrc' alt="">
+    <!-- </div> -->
+    <span>商品名称:{{data.name}}</span>
+    <span>价格:{{data.price}}</span>
   </div>
 </template>
 
 <script>
 import '@/assets/css/goodscard.css'
+import Mock from 'mockjs'
 export default {
   props: ['data'],
   data () {
     return {
-
+      imgSrc: ''
     }
+  },
+  mounted: function () {
+    this.imgSrc = Mock.Random.dataImage('300x350', this.data.name)
   }
 }
 </script>
