5ef046f9dce64a3da04e2fd5df29e94b7286fda5
Attention355 <823727097@qq.com>
2019-11-19 20:16:48 +0800

    商品卡片组建及样式

---
+++ src/components/goodscard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="goodsCard">
+    <div class='goodsImg' :style='"background:"+data.color'>
+    </div>
+    <span>{{data.name}}</span>
+    <span>{{data.price}}</span>
+  </div>
+</template>
+
+<script>
+import '@/assets/css/goodscard.css'
+export default {
+  props: ['data'],
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
